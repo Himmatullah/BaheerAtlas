8de0830e5de0a8c20275492c112da9113c1738b5
Himmatullah <h.tarakai12@gmail.com>
2024-12-16 11:54:29 +0430

    More pages been added

---
+++ about.php
@@ -0,0 +1,44 @@
+<?php require_once("header.php");
+?>
+              <h2>About</h2>
+              <p>Baheer Atlas Logistic Service Company, founded in 2022 with registration number
+                157615 and license number 82589, operates under the purview of the Ministry of
+                Industry and Commerce.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div><!-- End Breadcrumbs -->
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row gy-4">
+          <div class="col-lg-6 position-relative align-self-start order-lg-last order-first">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 content order-last  order-lg-first">
+            <h3>About Us</h3>
+            <p style="text-align: justify;">
+              Baheer Atlas Logistic Service Company, founded in 2022 with registration number
+              157615 and license number 82589, operates under the purview of the Ministry of
+              Industry and Commerce. As a dynamic player in the logistics industry, the company
+              is committed to providing comprehensive and efficient services to meet the diverse
+              needs of its clients. With a focus on excellence and innovation, Baheer Atlas
+              Logistic strives to streamline the supply chain, offering reliable transportation,
+              warehousing, and distribution solutions. The company's dedication to quality and
+              adherence to industry regulations make it a trusted partner for businesses seeking
+              seamless and cost-effective logistics support. As it continues to grow and evolve,
+              Baheer Atlas Logistic remains dedicated to contributing to the success and
+              sustainability of its clients through tailored and reliable logistical services.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+<?php require_once("footer.php");
+?>
---
+++ footer.php
@@ -0,0 +1,76 @@
+<!-- ======= Stats Counter Section ======= -->
+<section id="stats-counter" class="stats-counter pt-0">
+      <div class="container" data-aos="fade-up">
+
+      </div>
+    </section><!-- End Stats Counter Section -->
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="container">
+      <div class="row gy-4">
+        <div class="col-lg-5 col-md-12 footer-info">
+          <a href="index.html" class="logo d-flex align-items-center">
+            <span>Baheer Atlas</span>
+          </a>
+          <p style="text-align: justify;">Baheer Atlas Logistic Service Company, founded in 2022 with registration number 157615 and 
+            license number 82589, operates under the purview of the Ministry of Industry and Commerce.</p>
+
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="services.php">Services</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+          <h4>Contact Us</h4>
+          <p>
+            Shahr-e-Naw<br>
+            Kabul<br>
+            Afghanistan<br><br>
+            <strong>Phone:</strong> +93 77 718 2343, +93785414848<br>
+            <strong>Email:</strong> baheer.atlas@gmail.com<br>
+          </p>
+
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container mt-4">
+      <div class="copyright">
+        &copy; All Rights Reserved with Baheer Atlas Logistic Service Company
+      </div>
+    
+    </div>
+
+  </footer><!-- End Footer -->
+  <!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
